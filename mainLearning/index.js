cf5b7b4ed051e76b30f1d255ed3c148f20eb7681
Vizenp <kalinskyvi@gmail.com>
2022-01-07 15:38:19 +0300

    objects

--- mainLearning/index.js
+++ mainLearning/index.js
@@ -169,8 +169,25 @@ var firstName = myName. slice (0, myName. indexOf (" "));
 var name = firstName.concat(" ", lastName);//добавляет одно к другому
 
 let ask = (question, yes, no) => (confirm(question)) ? yes() : no()*/
-let user = {};
 
-user.likes = true;
+// до вызова функции
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+}
 
-console.log(user);
+multiplyNumeric(menu)
+
+/* после вызова функции
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};  */
+function multiplyNumeric(obj){
+  for(let key in obj){
+    obj[key] = (typeof obj[key] == 'number') ? obj[key] * 2 : obj[key];
+  }
+}
+console.log(menu)
